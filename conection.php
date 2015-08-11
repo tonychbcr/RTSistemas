5c03d6d2fcb536019e03376e0ea916b92c3a0270
tonychbcr <tonychb@gmail.com>
2015-08-11 15:59:38 -0600

    Update conection.php

--- conection.php
+++ conection.php
@@ -1,16 +1,9 @@
 <?
-// $host="localhost";
-//$username="plantacion";
-//$password="Testing2-3";
-//$database="plantacion";
-//$connection = mysql_connect($host,$username,$password);
-//mysql_select_db($database,$connection);
-//session_start();
-
-$host="ppmanagerx.db.8692754.hostedresource.com";
-$username="ppmanagerx";
-$password="Ppm123#456";
-$database="ppmanagerx";
+/*Conteccion a base de datos Mysql*/
+$host="databasename";
+$username="databaseuser";
+$password="databasepass";
+$database="databasename";
 $connection = mysql_connect($host,$username,$password);
 mysql_select_db($database,$connection);
 //session_start();

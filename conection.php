122ec155f83d708efebba73e4a4bbdf494f2c6f6
tonychbcr <tonychb@gmail.com>
2015-08-11 15:55:56 -0600

    Create conection.php

---
+++ conection.php
@@ -0,0 +1,17 @@
+<?
+// $host="localhost";
+//$username="plantacion";
+//$password="Testing2-3";
+//$database="plantacion";
+//$connection = mysql_connect($host,$username,$password);
+//mysql_select_db($database,$connection);
+//session_start();
+
+$host="ppmanagerx.db.8692754.hostedresource.com";
+$username="ppmanagerx";
+$password="Ppm123#456";
+$database="ppmanagerx";
+$connection = mysql_connect($host,$username,$password);
+mysql_select_db($database,$connection);
+//session_start();
+?>

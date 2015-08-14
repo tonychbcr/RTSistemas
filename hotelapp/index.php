dd5a16d0b6f1964ca8f87ea78a361069ce1d904f
tonychbcr <tonychb@gmail.com>
2015-08-13 22:26:15 -0600

    Hotel App
    Hotel APP

---
+++ hotelapp/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hotel App-Login</title>
+<!--Metatags-->
+<meta name="description" content="Hotel App Login" />
+<meta name="keywords" content="Hotel App Login" />
+<meta name="robots" content="index,follow" />
+<!--Metatags-->
+<!--Archivo de Estilos-->
+<link href="ppcire.css" rel="stylesheet" type="text/css">
+<!--Archivo de Estilos-->
+</head>
+<body>
+<div id="container"><!--Contenedor Principal-->
+  <div id="header">
+  Encabezado
+    <div class="nav">
+    	<ul>
+         <li class="home"><a href="">Home</a></li>
+         <li class="news"><a href="">About</a></li>
+         <li class="contact"><a href="">Contact</a></li>
+        </ul>
+    </div>
+  </div>
+<div id="maincontent">
+<form action="validate.php"  method="post">
+<label>Usuario:</label><input type="text" name="vusuario">
+<label>Contraseña:</label><input type="text" name="vusuario">
+<input type="submit">
+</form>
+</div>
+<footer id="footer">
+<nav class="footernav">
+   <ul>
+     <li class="home"><a href="">Home</a></li>
+     <li class="news"><a href="">About</a></li>
+     <li class="contact"><a href="">Contact</a></li>
+   </ul>
+</nav>
+<div id="footerlink">Copyright 2015 - Guanasoft.Net</div>
+</footer>
+</div>
+</body>
+</html>
